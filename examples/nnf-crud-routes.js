10f0b9f2846c5cca7bb9a176d45d51b10105bcd1
zatosinternet <diego@zatos.com.br>
2020-11-11 18:12:03 -0300

    Update nnf-crud-routes.js

--- examples/nnf-crud-routes.js
+++ examples/nnf-crud-routes.js
@@ -1,6 +1,9 @@
 ## NetOffice Node Function
 
 ***Exemplo de rotas***	
+	
+URL: https://broker.n.wop.net.br:3095/route/1504903cf70369b8145917e0c752e46f/index
+
 ```javascript	
 var ROUTE = req.params.route;
 
